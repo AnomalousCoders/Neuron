89e46a30030863ba2e685c596cd8a3348abe6072
Helight <helightdev@gmail.com>
2022-06-14 21:56:59 +0200

    Patcher Module

---
+++ Neuron.Modules.Patcher/PatcherModule.cs
@@ -0,0 +1,31 @@
+﻿using Neuron.Core.Module;
+using Ninject;
+
+namespace Neuron.Modules.Patcher
+{
+    [Module(
+        Name = "Patcher",
+        Description = "Neuron Patcher Module"
+    )]
+    public class PatcherModule : Module
+    {
+        
+        [Inject] 
+        public IPatcherService Patcher { get; set; }
+
+        public override void Load()
+        {
+            Logger.Information("Loading PatcherModule");
+        }
+
+        public override void Enable()
+        {
+            Logger.Information("Enabling PatcherModule");
+        }
+
+        public override void Disable()
+        {
+            Logger.Information("Disabling PatcherModule");
+        }
+    }
+}
